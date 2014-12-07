93c99a0d8be2a492bad88a1b25726fba7e629c25
engineer-murataydin <murat_aydin14@hotmail.com>
2014-12-07 17:45:08 +0100

    week 4 in progress

---
+++ week4/LineareAlgebraWeek4/LineareAlgebra/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineareAlgebraWeek2
+{
+    class Product
+    {
+        public Product()
+        {
+
+        }
+
+        static double inproduct(MyVector a, MyVector b)
+        {
+
+        }
+
+        static MyVector uitproduct(MyVector a, MyVector b)
+        {
+
+        }
+    }
+}
